9e6545391575321b58a18a7835c6b4dabac33e54
Dustin Schau <dustinschau@gmail.com>
2019-06-21 13:54:39 -0500

    chore: fix maybe

--- src/utils/convert-json-to-markdown.js
+++ src/utils/convert-json-to-markdown.js
@@ -2,11 +2,19 @@ const json2md = require("json2md")
 const YAML = require("yamljs")
 const matter = require("gray-matter")
 
+const normalize = markdown => {
+  if (markdown.indexOf("---") === -1) {
+    return markdown
+  }
+
+  return markdown.replace(/---([\s\S]+)---/, (_, group) => {
+    return `---\n${group.trim().replace(/\n+/g, "\n")}\n---`
+  })
+}
+
 module.exports = ({content, file}) => {
   const {content: markdown, data: frontmatter} = matter(
-    json2md(content)
-      .replace(/\n{2,}/g, "\n")
-      .trim()
+    json2md(normalize(content))
   )
   return `---
 ${YAML.stringify(Object.assign({}, file, frontmatter))}---
