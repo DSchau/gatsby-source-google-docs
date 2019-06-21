5537e0d1356c1f443eba190f55b84c769482b363
Dustin Schau <dustinschau@gmail.com>
2019-06-21 13:44:04 -0500

    chore: fix

--- src/utils/convert-json-to-markdown.js
+++ src/utils/convert-json-to-markdown.js
@@ -3,7 +3,11 @@ const YAML = require("yamljs")
 const matter = require("gray-matter")
 
 module.exports = ({content, file}) => {
-  const {content: markdown, data: frontmatter} = matter(json2md(content))
+  const {content: markdown, data: frontmatter} = matter(
+    json2md(content)
+      .replace(/\n{2,}/g, "\n")
+      .trim()
+  )
   return `---
 ${YAML.stringify(Object.assign({}, file, frontmatter))}---
 
